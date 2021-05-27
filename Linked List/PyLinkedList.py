90590d4c741ed356eea3a822ca9820a95fd21b30
kdotwood1 <81205156+kdotwood1@users.noreply.github.com>
2021-05-27 19:26:33 +1200

    add method complete and part of remove method

--- Linked List/PyLinkedList.py
+++ Linked List/PyLinkedList.py
@@ -1,10 +1,23 @@
 class PyLinkedList:
-   def __init__ (self):
-      
-
-class Node:
 # ###########################
-# Constructor
-   def __init__ (self, key, node):
+# Class Variables
+   head = Node("")
+
+# ###########################
+# Public Methods   
+   def add(self, key):
+      body = Node(key)
+      body.next = head
+      head = body
+      return
+   
+   def remove(self, key):
+      temp = head
+      while temp.key
+
+class Node(PyLinkedList):
+   # ###########################
+   # Constructor
+   def __init__ (self, key):
       self.key = key
-      self.node = node
+      self.next = null
