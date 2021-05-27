10b04499f4b99b23d24e9a1c737056ff821bfbbf
kdotwood1 <81205156+kdotwood1@users.noreply.github.com>
2021-05-27 18:23:54 +1200

    wrote the node class for the linked list

---
+++ Linked List/PyLinkedList.py
@@ -0,0 +1,10 @@
+class PyLinkedList:
+   def __init__ (self):
+      
+
+class Node:
+# ###########################
+# Constructor
+   def __init__ (self, key, node):
+      self.key = key
+      self.node = node
